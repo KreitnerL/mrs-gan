b120af848913fe9dafc9768d6dc996d054c74e9b
Linus Kreitner <linus.kreitner@tum.de>
2020-10-19 20:06:32 +0200

    fix: make random forest runable

--- random_forest/random_forest.py
+++ random_forest/random_forest.py
@@ -5,6 +5,9 @@ from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import time
+import datetime
+from joblib import dump, load
 
 class RandomForest:
     """
@@ -29,8 +32,22 @@ class RandomForest:
         - x: List of spectra. N1 x L double, N1 = number of spectra, L length of spectra
         - y: List of quantifications. M x N2, M = number of metabolites, N2 = number of spectra
         """
-        print('training random forest with {0} trees on {1} data samples', self.num_trees, len(x))
+        print('training random forest with {0} trees on {1} data samples...'.format(self.num_trees, len(x)))
+        start = time.time()
         self.regressor.fit(x, y)
+        print('training completed in {:.3f} sec'.format(time.time()-start))
+            
+
+    def store(self, path):
+        filename = 'RF_' + str(datetime.datetime.now()).replace(':','-').replace(' ','_').replace('.','_') + '.joblib'
+        filepath = os.path.join(path, filename)
+        print('storing random forest weights at', filepath)
+        dump(self.regressor, filepath)
+
+    def load(self, filepath):
+        print('Loading pretrained model from', filepath)
+        self.regressor = load(filepath)
+
 
     def test(self, x) -> list:
         """
@@ -43,7 +60,10 @@ class RandomForest:
         -------
         prediction : The predicted values. N2 x M
         """
-        return self.regressor.predict(x)
+        start = time.time()
+        prediction =  self.regressor.predict(x)
+        print('prediction of', len(x), 'samples completed in {:.3f} sec'.format(time.time()-start))
+        return np.array(prediction)
 
     def compute_error(self, predictions: list, y):
         """
@@ -61,13 +81,13 @@ class RandomForest:
         """
         err_rel = []
         avg_err_rel = []
-        for metabolite in len(self.labels):
-            err_rel[metabolite] = (abs(predictions[metabolite] - y[metabolite])) / (abs(y[metabolite]))
-            avg_err_rel[metabolite] = np.mean(err_rel[metabolite], axis=2)
-            print('Relative error {0}: {1}\n', self.labels(metabolite), avg_err_rel[metabolite])
+        for metabolite in range(len(self.labels)):
+            err_rel.append((abs(predictions[:,metabolite] - y[:,metabolite])) / (abs(y[:,metabolite])))
+            avg_err_rel.append(np.mean(err_rel[metabolite]))
+            print('Average Relative Error {0}: {1}\n'.format(self.labels[metabolite], avg_err_rel[metabolite]))
         return err_rel, avg_err_rel
 
-    def save_plot(self, err_rel, path: str):
+    def save_plot(self, err_rel, path: str, max_y = 1):
         """
         Save a boxplot from the given relative errors.
 
@@ -83,6 +103,7 @@ class RandomForest:
         plt.boxplot(err_rel, notch = True, labels=self.labels)
         plt.ylabel('Relative Error')
         plt.title('Error per predicted metabolite')
+        plt.gca().set_ylim([0,max_y])
         path = os.path.join(path, 'rel_err_boxplot.png')
         plt.savefig(path, format='png')
         plt.cla()
@@ -93,6 +114,8 @@ def train_val(x_train, x_test, y_train, y_test, labels, path, num_trees=100):
     """
     rf = RandomForest(num_trees, labels)
     rf.train(x_train, y_train)
+    rf.store(path)
     predictions = rf.test(x_test)
     err_rel, avg_err_rel = rf.compute_error(predictions, y_test)
-    rf.save_plot(err_rel, path)
+    max_y = min(max(np.array(avg_err_rel)+0.2),1)
+    rf.save_plot(err_rel, path, max_y)
