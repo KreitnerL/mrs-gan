4d66fe79c22f5fd2d3955982d7f85f11d3585af3
Linus Kreitner <linus.kreitner@tum.de>
2021-03-30 23:11:12 +0200

    refactor: remove assets

--- debugging_scripts/compute_layer_output.py
+++
@@ -1,4 +0,0 @@
-def conv_output(L_in, padding=0, dilation=1, kernelsize=1, stride=1):
-    return int(((L_in + 2 * padding - dilation*(kernelsize-1)-1)/stride) +1)
-
-print(conv_output(256, kernelsize=3, padding=1, stride=1))
