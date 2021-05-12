fix: fix lr_scheduler and fid scaling

generate_spectra.py
@@ -29,13 +29,13 @@ pm = MRSPhysicsModel(opt).cuda()
 if args.quantitity_path:
     quantitites = io.loadmat(args.quantitity_path)
     concentrations = []
-    concentrations.append(torch.from_numpy(quantitites['cho'][:,:args.N]/(3.6)))
+    concentrations.append(torch.from_numpy(quantitites['cho'][:,:args.N]/(3.5)))
     concentrations.append(torch.ones((1,args.N), dtype=torch.float64))
-    concentrations.append(torch.from_numpy(quantitites['naa'][:,:args.N]/(3.6)))
+    concentrations.append(torch.from_numpy(quantitites['naa'][:,:args.N]/(3.5)))
     concentrations = torch.cat(concentrations, dim=0).transpose(0,1)
     args.N = concentrations.shape[1]
 else:
-    concentrations = (3.6 - 0.01) * torch.rand(args.N,3) + 0.01
+    concentrations = (3.5 - 0.01) * torch.rand(args.N,3) + 0.01
 
 start_time = time.time()
 spectra_batches = []

plot_spectra.py
@@ -61,7 +61,7 @@ def plotIdeal():
 
     plt.figure()
     for index,i in enumerate(indices):
-        params = torch.tensor([[cho[i], naa[i]]]).cuda()/3.6
+        params = torch.tensor([[cho[i], naa[i]]]).cuda()/3.5
         spectrum = np.squeeze(pm.forward(params).detach().cpu().numpy())
         plt.cla()
         plt.plot(x,spectrum.transpose())

validation_networks/MLP/MLP.py
@@ -60,7 +60,7 @@ class MLP():
         pred = []
         for spectra, _ in val_dataset:
             self.input.resize_(spectra.size()).copy_(spectra)
-            pred.append((self.network.forward(self.input)*3.6).detach().cpu().numpy())
+            pred.append((self.network.forward(self.input)*3.5).detach().cpu().numpy())
 
         return np.concatenate(pred)
         
@@ -90,7 +90,7 @@ class MLP():
                 total_iters += self.batch_size
                 self.input.resize_(spectra.size()).copy_(spectra)
                 self.label.resize_(labels.size()).copy_(labels)
-                pred = self.network.forward(self.input)*3.6
+                pred = self.network.forward(self.input)*3.5
                 loss.append(self.backward(pred, self.label))
 
                 if total_iters%self.validate_every==0: